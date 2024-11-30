9a5be28966c076f5042581c5c4416666c15d0110
97mams <97mams@gmail.com>
2024-11-30 20:02:02 +0300

    change loading to spinner

--- app/admin/citation/new/loading.tsx
+++ app/admin/citation/new/loading.tsx
@@ -3,7 +3,7 @@ import { LoaderCircleIcon } from "lucide-react";
 export default function Loading() {
     return (
         <div >
-            <LoaderCircleIcon />
+            <LoaderCircleIcon className="animate-spin mx-auto mt-8" />
         </div>
     )
 }
